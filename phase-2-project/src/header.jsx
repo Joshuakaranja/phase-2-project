953cf0fb33a6d5f67a277735a88fba917f574c73
Pascal <pascaldenzel7@gmail.com>
2025-07-17 11:59:27 +0300

    initial

---
+++ phase-2-project/src/header.jsx
@@ -0,0 +1,10 @@
+function Body() {
+  return (
+    <div className="body">
+      <h1>Welcome to the Body Component</h1>
+      <p>This is where the main content of the application will be displayed.</p>
+    </div>
+  );
+}
+
+export default Body;
